471dc627b9c7c2a76a8e0ce31ec492a6371efa89
Cianuro-83 <buzzeosimone@yahoo.com>
2023-03-13 14:14:31 +0100

    set: font family

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,10 @@
-<template></template>
+<template>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis facilis
+  quod, alias dolores vero totam molestias nemo dolorem hic iste assumenda
+  consequatur eius ea sapiente nesciunt autem quibusdam odit illo laboriosam
+  aut! Temporibus, voluptatum molestias. In, eos maxime dolore nesciunt minus
+  est nihil nobis aliquid ab cum, ducimus atque! Rerum.
+</template>
 
 <script></script>
 
