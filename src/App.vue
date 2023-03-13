321ed7a8f26d8db46a6f50299aab9e8ebc4f95fb
Cianuro-83 <buzzeosimone@yahoo.com>
2023-03-13 17:30:12 +0100

    FEAT: Componente CardBlock

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis facilis
-  quod, alias dolores vero totam molestias nemo dolorem hic iste assumenda
-  consequatur eius ea sapiente nesciunt autem quibusdam odit illo laboriosam
-  aut! Temporibus, voluptatum molestias. In, eos maxime dolore nesciunt minus
-  est nihil nobis aliquid ab cum, ducimus atque! Rerum.
+  <CardBlock />
 </template>
 
-<script></script>
+<script>
+import CardBlock from "./components/CardBlock.vue";
+export default {
+  components: {
+    CardBlock,
+    // **************************
+    // FINE COMPONETS
+  },
+  // **************************
+};
+</script>
 
 <style lang="scss">
 @use "./style/general.scss";
---
+++ src/components/CardBlock.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="block-container">
+    <img
+      class="card"
+      v-for="(block, i) in blocks"
+      :key="i"
+      :src="block"
+      alt=""
+    />
+    <div class="apici">
+      <img class="palloncino" src="/svg/svg-7.svg" alt="" />
+      <img class="shop" src="/svg/svg-6.svg" alt="" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    // **************************
+    // FINE COMPONETS
+  },
+  // **************************
+  data() {
+    return {
+      blocks: [
+        `/img/h-2-port-img-1.jpg`,
+        `/img/h-2-port-img-2.jpg`,
+        `/img/h-2-port-img-3.jpg`,
+        `/img/h-2-port-img-4.jpg`,
+        `/img/h-2-port-img-5.jpg`,
+        `/img/h-2-port-img-6.jpg`,
+      ],
+      // **************************
+      // FINE DATA E RETURN &
+    };
+  },
+  // **************************
+  computed: {
+    // **************************
+    // FINE COMPUTED
+  },
+  // **************************
+  watch: {
+    // **************************
+    // FINE WATCH
+  },
+  // **************************
+  methods: {
+    // **************************
+    // FINE METHODS
+  },
+  // **************************
+};
+</script>
+
+<style lang="scss" scoped>
+@use "../style/partials//variables" as *;
+.block-container {
+  display: flex;
+  flex-wrap: wrap;
+  position: relative;
+  .card {
+    width: calc(100% / 3);
+    cursor: pointer;
+  }
+  .palloncino,
+  .shop {
+    width: 48px;
+    padding: 9px;
+    cursor: pointer;
+  }
+  .apici {
+    position: absolute;
+    right: 0;
+    top: 134px;
+  }
+  .palloncino {
+    background-color: red;
+    margin-bottom: 9px;
+  }
+  .shop {
+    background-color: white;
+  }
+}
+</style>
