2b1ebcd3435876921b3ab50ff98c75851c628818
Cianuro-83 <buzzeosimone@yahoo.com>
2023-03-13 13:59:31 +0100

    IMPOSTAZIONE VITE, BOOTSTRAP, POPPERJS E SASS

---
+++ src/components/Template.vue
@@ -0,0 +1,39 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  components: {
+    // **************************
+    // FINE COMPONETS
+  },
+  // **************************
+  data() {
+    return {
+      // **************************
+      // FINE DATA E RETURN &
+    };
+  },
+  // **************************
+  computed: {
+    // **************************
+    // FINE COMPUTED
+  },
+  // **************************
+  watch: {
+    // **************************
+    // FINE WATCH
+  },
+  // **************************
+  methods: {
+    // **************************
+    // FINE METHODS
+  },
+  // **************************
+};
+</script>
+
+<style lang="scss" scoped>
+@use "../style/partials//variables" as *;
+</style>
